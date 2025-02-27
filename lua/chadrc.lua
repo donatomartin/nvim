e368b6bc274c2d509bb17ac2e1f48b6d791d9dce
donatomartin <uo288787@uniovi.es>
2025-02-27 16:28:38 +0100

    experience improved

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -9,11 +9,11 @@ M.base46 = {
 	-- },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.nvdash = { load_on_startup = true }
+M.ui = {
+      tabufline = {
+         lazyload = false
+     }
+}
 
 return M

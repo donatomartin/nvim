bbcdd202e66a1bf10b451b86492918c17e3b3575
donatomartin <uo288787@uniovi.es>
2025-03-02 01:00:55 +0100

    Mappings ordered

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,27 +3,47 @@
 -- NVChad Mappings
 local map = vim.keymap.set
 
+-- Cheatsheet
+map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "toggle nvcheatsheet" })
+
+-- Insert Mode Move
 map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
 map("i", "<C-e>", "<End>", { desc = "move end of line" })
+
 map("i", "<C-h>", "<Left>", { desc = "move left" })
 map("i", "<C-l>", "<Right>", { desc = "move right" })
 map("i", "<C-j>", "<Down>", { desc = "move down" })
 map("i", "<C-k>", "<Up>", { desc = "move up" })
 
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+-- Switch Windows
+map("n", "<C-h>", "<C-w>h", { desc = "Window switch left" })
+map("n", "<C-l>", "<C-w>l", { desc = "Window switch right" })
+map("n", "<C-j>", "<C-w>j", { desc = "Window switch down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Window switch up" })
 
+-- Resize Windows
+map("n", "<left>", ":vertical resize +5<CR>", { desc = "Window resize left" })
+map("n", "<right>", ":vertical resize -5<CR>", { desc = "Window resize right" })
+map("n", "<down>", ":horizontal resize -5<CR>", { desc = "Window resize down" })
+map("n", "<up>", ":horizontal resize +5<CR>", { desc = "Window resize up" })
+
+-- Window close
+map("n", "<leader>x", ":q<CR>", { desc = "Window close" })
+
+-- Search Highlighting
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
 
-map({"n", "i"}, "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
+-- Save
+map({ "n", "i" }, "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
+
+-- Copy
 map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
 
+-- Number
 map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
 map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
-map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "toggle nvcheatsheet" })
 
+-- Conform Format
 map("n", "<leader>fm", function()
   require("conform").format { lsp_fallback = true }
 end, { desc = "general format file" })
@@ -32,8 +52,6 @@ end, { desc = "general format file" })
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
 
 -- tabufline
-map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
-
 map("n", "<tab>", function()
   require("nvchad.tabufline").next()
 end, { desc = "buffer goto next" })
@@ -41,14 +59,6 @@ end, { desc = "buffer goto next" })
 map("n", "<S-tab>", function()
   require("nvchad.tabufline").prev()
 end, { desc = "buffer goto prev" })
-
-map("n", "<leader>x", function()
-  require("nvchad.tabufline").close_buffer()
-end, { desc = "buffer close" })
-
--- Comment
-map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
-map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
 
 -- nvimtree
 map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
@@ -64,12 +74,13 @@ map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = 
 map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
 map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
 map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
-map("n", "<leader>cn", "<cmd>edit $MYVIMRC <CR> <cmd>cd " .. vim.fn.stdpath('config') .. " <CR>", {desc = "telescope open config"})
-
-map("n", "<leader>th", function()
-  require("nvchad.themes").open()
-end, { desc = "telescope nvchad themes" })
-
+map(
+  "n",
+  "<leader>cn",
+  "<cmd>edit $MYVIMRC <CR> <cmd>cd " .. vim.fn.stdpath "config" .. " <CR>",
+  { desc = "telescope open config" }
+)
+map("n", "<leader>fp", ":Telescope project<CR>", { noremap = true, silent = true, desc = "Telescope Project" })
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
 map(
   "n",
@@ -78,30 +89,27 @@ map(
   { desc = "telescope find all files" }
 )
 
--- terminal
-map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+-- Themes
+map("n", "<leader>th", function()
+  require("nvchad.themes").open()
+end, { desc = "NVChad themes" })
 
--- new terminals
-map("n", "<leader>h", function()
-  require("nvchad.term").new { pos = "sp" }
-end, { desc = "terminal new horizontal term" })
 
-map("n", "<leader>v", function()
-  require("nvchad.term").new { pos = "vsp" }
-end, { desc = "terminal new vertical term" })
+-- Exit Terminal Mode
+map("t", "<Esc>", "<C-\\><C-N>", { desc = "Terminal escape terminal mode" })
 
--- toggleable
+-- Toggle Terminal
 map({ "n", "t" }, "<A-v>", function()
   require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
-end, { desc = "terminal toggleable vertical term" })
+end, { desc = "Terminal toggle vertical" })
 
 map({ "n", "t" }, "<A-h>", function()
   require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
-end, { desc = "terminal toggleable horizontal term" })
+end, { desc = "Terminal toggle horizontal" })
 
 map({ "n", "t" }, "<A-i>", function()
   require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
-end, { desc = "terminal toggle floating term" })
+end, { desc = "Terminal toggle floating" })
 
 -- whichkey
 map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
@@ -109,11 +117,6 @@ map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
 map("n", "<leader>wk", function()
   vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
 end, { desc = "whichkey query lookup" })
-
--- My Mappings
-
-map("i", "jk", "<ESC>")
-map("t", "jk", [[<C-\><C-n>]])
 
 -- Map a key to execute commands :!
 map(
@@ -123,30 +126,10 @@ map(
   { noremap = true, silent = true, desc = "Terminal execute command" }
 )
 
--- Map a key to accept Copilot suggestions
-map("i", "<M-l>", function()
-  vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
-end, { desc = "Copilot Accept", noremap = true, silent = true })
-
--- Map a key to open telescope project
-map("n", "<leader>fp", ":Telescope project<CR>", { noremap = true, silent = true, desc = "Telescope Project" })
-
--- True Vim Experience
-map({ "n", "i", "v" }, "<left>", '<cmd>echo "Use h to move!!"<CR>')
-map({ "n", "i", "v" }, "<right>", '<cmd>echo "Use l to move!!"<CR>')
-map({ "n", "i", "v" }, "<up>", '<cmd>echo "Use k to move!!"<CR>')
-map({ "n", "i", "v" }, "<down>", '<cmd>echo "Use j to move!!"<CR>')
-
--- Keep System Clipboard
-map({ "i", "v" }, "<C-c>", '"+y', { noremap = true, silent = true, desc = "Clipboard Copy to clipboard" })
-map({ "n", "i", "v", "t" }, "<C-v>", '"+p', { noremap = true, silent = true, desc = "Clipboard Paste from clipboard" })
-
 -- Exit Vim
-
 map("n", "<leader>q", ":q<CR>", { noremap = true, silent = true, desc = "General Quit" })
 
 -- Tmux Navigation
-
 map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { noremap = true, silent = true, desc = "Tmux Move Left" })
 map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { noremap = true, silent = true, desc = "Tmux Move Down" })
 map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { noremap = true, silent = true, desc = "Tmux Move Up" })

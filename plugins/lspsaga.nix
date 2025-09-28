840ec6bdcfccd557ac37cb3dfbfc73b18d3447b3
donatomartin <uo288787@uniovi.es>
2025-09-28 23:13:29 +0200

    feat: update lspsaga declarations

--- plugins/lspsaga.nix
+++ plugins/lspsaga.nix
@@ -1,192 +1,89 @@
 {
   plugins.lspsaga = {
     enable = true;
-    beacon = {
-      enable = true;
-    };
-    ui = {
-      border = "rounded"; # One of none, single, double, rounded, solid, shadow
-      codeAction = "💡"; # Can be any symbol you want 💡
-    };
-    hover = {
-      openCmd = "!floorp"; # Choose your browser
-      openLink = "gx";
-    };
-    diagnostic = {
-      borderFollow = true;
-      diagnosticOnlyCurrent = false;
-      showCodeAction = true;
-    };
-    symbolInWinbar = {
-      enable = true; # Breadcrumbs
-    };
-    codeAction = {
-      extendGitSigns = false;
-      showServerName = true;
-      onlyInCursor = true;
-      numShortcut = true;
-      keys = {
-        exec = "<CR>";
-        quit = [
-          "<Esc>"
-          "q"
-        ];
+
+    settings = {
+      beacon.enable = true;
+
+      ui = {
+        border = "rounded";         # none|single|double|rounded|solid|shadow
+        code_action = "💡";         # renombrado
       };
-    };
-    lightbulb = {
-      enable = false;
-      sign = false;
-      virtualText = true;
-    };
-    implement = {
-      enable = false;
-    };
-    rename = {
-      autoSave = false;
-      keys = {
-        exec = "<CR>";
-        quit = [
-          "<C-k>"
-          "<Esc>"
-        ];
-        select = "x";
+
+      hover = {
+        open_cmd = "!floorp";       # si te falla, prueba sin "!" → "floorp"
+        open_link = "gx";
       };
-    };
-    outline = {
-      autoClose = true;
-      autoPreview = true;
-      closeAfterJump = true;
-      layout = "normal"; # normal or float
-      winPosition = "right"; # left or right
-      keys = {
-        jump = "e";
-        quit = "q";
-        toggleOrJump = "o";
+
+      diagnostic = {
+        border_follow = true;
+        diagnostic_only_current = false;
+        show_code_action = true;
       };
-    };
-    scrollPreview = {
-      scrollDown = "<C-f>";
-      scrollUp = "<C-b>";
+
+      symbol_in_winbar.enable = true;
+
+      code_action = {
+        extend_git_signs = false;
+        show_server_name = true;
+        only_in_cursor = true;
+        num_shortcut = true;
+        keys = {
+          exec = "<CR>";
+          quit = [ "<Esc>" "q" ];
+        };
+      };
+
+      lightbulb = {
+        enable = false;
+        sign = false;
+        virtual_text = true;
+      };
+
+      implement.enable = false;
+
+      rename = {
+        auto_save = false;
+        keys = {
+          exec = "<CR>";
+          quit = [ "<C-k>" "<Esc>" ];
+          select = "x";
+        };
+      };
+
+      outline = {
+        auto_close = true;
+        auto_preview = true;
+        close_after_jump = true;
+        layout = "normal";          # normal|float
+        win_position = "right";     # left|right
+        keys = {
+          jump = "e";
+          quit = "q";
+          toggle_or_jump = "o";
+        };
+      };
+
+      scroll_preview = {
+        scroll_down = "<C-f>";
+        scroll_up = "<C-b>";
+      };
     };
   };
+
   keymaps = [
-    {
-      mode = "n";
-      key = "gd";
-      action = "<cmd>Lspsaga finder def<CR>";
-      options = {
-        desc = "Goto Definition";
-        silent = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "gr";
-      action = "<cmd>Lspsaga finder ref<CR>";
-      options = {
-        desc = "Goto References";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "gD";
-      action = "<cmd>Lspsaga show_line_diagnostics<CR>";
-      options = {
-        desc = "Goto Declaration";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "gI";
-      action = "<cmd>Lspsaga finder imp<CR>";
-      options = {
-        desc = "Goto Implementation";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "gT";
-      action = "<cmd>Lspsaga peek_type_definition<CR>";
-      options = {
-        desc = "Type Definition";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "K";
-      action = "<cmd>Lspsaga hover_doc<CR>";
-      options = {
-        desc = "Hover";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "<leader>cw";
-      action = "<cmd>Lspsaga outline<CR>";
-      options = {
-        desc = "Outline";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "<leader>cr";
-      action = "<cmd>Lspsaga rename<CR>";
-      options = {
-        desc = "Rename";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "<leader>ca";
-      action = "<cmd>Lspsaga code_action<CR>";
-      options = {
-        desc = "Code Action";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "<leader>cd";
-      action = "<cmd>Lspsaga show_line_diagnostics<CR>";
-      options = {
-        desc = "Line Diagnostics";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "[d";
-      action = "<cmd>Lspsaga diagnostic_jump_next<CR>";
-      options = {
-        desc = "Next Diagnostic";
-        silent = true;
-      };
-    }
-
-    {
-      mode = "n";
-      key = "]d";
-      action = "<cmd>Lspsaga diagnostic_jump_prev<CR>";
-      options = {
-        desc = "Previous Diagnostic";
-        silent = true;
-      };
-    }
+    { mode = "n"; key = "gd"; action = "<cmd>Lspsaga finder def<CR>"; options = { desc = "Goto Definition"; silent = true; }; }
+    { mode = "n"; key = "gr"; action = "<cmd>Lspsaga finder ref<CR>"; options = { desc = "Goto References"; silent = true; }; }
+    { mode = "n"; key = "gD"; action = "<cmd>Lspsaga show_line_diagnostics<CR>"; options = { desc = "Goto Declaration"; silent = true; }; }
+    { mode = "n"; key = "gI"; action = "<cmd>Lspsaga finder imp<CR>"; options = { desc = "Goto Implementation"; silent = true; }; }
+    { mode = "n"; key = "gT"; action = "<cmd>Lspsaga peek_type_definition<CR>"; options = { desc = "Type Definition"; silent = true; }; }
+    { mode = "n"; key = "K";  action = "<cmd>Lspsaga hover_doc<CR>"; options = { desc = "Hover"; silent = true; }; }
+    { mode = "n"; key = "<leader>cw"; action = "<cmd>Lspsaga outline<CR>"; options = { desc = "Outline"; silent = true; }; }
+    { mode = "n"; key = "<leader>cr"; action = "<cmd>Lspsaga rename<CR>"; options = { desc = "Rename"; silent = true; }; }
+    { mode = "n"; key = "<leader>ca"; action = "<cmd>Lspsaga code_action<CR>"; options = { desc = "Code Action"; silent = true; }; }
+    { mode = "n"; key = "<leader>cd"; action = "<cmd>Lspsaga show_line_diagnostics<CR>"; options = { desc = "Line Diagnostics"; silent = true; }; }
+    { mode = "n"; key = "[d"; action = "<cmd>Lspsaga diagnostic_jump_next<CR>"; options = { desc = "Next Diagnostic"; silent = true; }; }
+    { mode = "n"; key = "]d"; action = "<cmd>Lspsaga diagnostic_jump_prev<CR>"; options = { desc = "Previous Diagnostic"; silent = true; }; }
   ];
 }
+

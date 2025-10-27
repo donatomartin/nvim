494170e310bb305dacbc74ce329feaa37e6d3016
donatomartin <uo288787@uniovi.es>
2025-10-27 16:46:08 +0100

    fix: correct oil creating weird directories

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,10 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
+  "stevearc/oil.nvim",
   opts = {},
-  -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,  -- replace netrw
+    })
+  end,
 }

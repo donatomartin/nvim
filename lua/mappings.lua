d41a5f096afd0e708596254529b84f8d268f075a
donatomartin <uo288787@uniovi.es>
2026-03-18 19:29:22 +0100

    feat: add copy file mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,6 @@
 local map = vim.keymap.set
 
+map({"n","v"}, "<leader>y", ":%y+<CR>", { desc = "Yank file to system clipboard" })
 map({"n","v"}, "<C-c>", ":%y+<CR>", { desc = "Yank file to system clipboard" })
 map("n", "<Esc>", ":noh<CR>", { desc = "Clear search hl" })
 map("t", "<Esc><Esc>", "<C-\\><C-N>", { desc = "Escape terminal mode" })

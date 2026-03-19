b149537c7c498f47073a41e2a3b67acc6a771fea
donatomartin <uo288787@uniovi.es>
2026-03-19 01:12:41 +0100

    feat: enable support for win32yank

--- lua/options.lua
+++ lua/options.lua
@@ -26,5 +26,23 @@ vim.o.pumheight = 10
 
 -- Set system clipboard
 vim.opt.clipboard = { "unnamed", "unnamedplus" }
+if vim.fn.has("wsl") == 1 then
+  local win32yank = "/mnt/c/Windows/System32/win32yank.exe"
+  if vim.fn.executable(win32yank) == 1 then
+    vim.g.clipboard = {
+      name = "win32yank (WSL)",
+      copy = {
+        ["+"] = { win32yank, "-i", "--crlf"},
+        ["*"] = { win32yank, "-i", "--crlf"},
+      },
+      paste = {
+        ["+"] = { win32yank, "-o", "--lf"},
+        ["*"] = { win32yank, "-o", "--lf"},
+      },
+      cache_enabled = 0,
+    }
+  end
+end
+
 
 vim.cmd("colorscheme catppuccin_mocha")

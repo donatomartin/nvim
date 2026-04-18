36184e887efe0039daddbcad1a6ef961e7903cca
donatomartin <uo288787@uniovi.es>
2026-04-19 01:40:06 +0200

    feat: ; now aliases :lua instead of :!

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,7 +3,7 @@ local map = vim.keymap.set
 map({"n","v"}, "<leader>y", ":%y+<cr>", { desc = "Yank file to system clipboard" })
 map({"n","v"}, "<C-c>", ":%y+<cr>", { desc = "Yank file to system clipboard" })
 map("t", "<Esc><Esc>", "<C-\\><C-N>", { desc = "Escape terminal mode" })
-map("n", ";", ":!")
+map("n", ";", ":lua ")
 map({"n", "v", "i", "t"}, "<F1>", "", { desc = "F1 is disabled" })
 map("n", "<Esc>", function ()
   vim.cmd("noh")

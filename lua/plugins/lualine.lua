09003334eb1cb4cd5d7cfbede01301ef8ae9e3d0
donatomartin <uo288787@uniovi.es>
2026-06-23 01:07:23 +0200

    feat: improve lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,4 +2,21 @@ vim.pack.add({
   'https://github.com/nvim-lualine/lualine.nvim',
 })
 
-require "lualine".setup()
+require("lualine").setup({
+  sections = {
+    lualine_a = { 'filename' },
+    lualine_b = { 'branch', 'diagnostics' },
+    lualine_c = { 'lsp_status' },
+    lualine_x = { 'encoding', 'fileformat', },
+    lualine_y = { 'filesize', 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = { 'diagnostics' },
+    lualine_c = { 'lsp_status' },
+    lualine_x = {},
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  }
+})
